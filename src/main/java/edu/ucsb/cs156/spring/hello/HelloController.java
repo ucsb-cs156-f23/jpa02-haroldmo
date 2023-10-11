3908a61930b7e8d8f05c1647606071021ea87f7c
Harold <haroldmo23@gmail.com>
2023-10-10 19:08:41 -0700

    updated test cases

--- src/main/java/edu/ucsb/cs156/spring/hello/HelloController.java
+++ src/main/java/edu/ucsb/cs156/spring/hello/HelloController.java
@@ -8,7 +8,7 @@ public class HelloController {
     
     @RequestMapping("/")
     public String index() {
-        return "Greetings from haroldmo! \n";
+        return "Greetings from haroldmo!";
     }
     
 }
